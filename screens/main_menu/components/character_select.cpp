d375a9c07acdc71ec437cd99b695562ff87772b9
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2026-05-19 19:58:53 +0200

    minor bug fix

--- screens/main_menu/components/character_select.cpp
+++ screens/main_menu/components/character_select.cpp
@@ -12,62 +12,55 @@ void init_char_select(MainMenuModule *main_menu_ptr)
     std::vector<std::string> avatar_names;
     std::vector<sf::Texture> avatar_images;
 
-    int n=1;//количество персонажей 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < avatar_names.size(); i++)
     {
-        Character ch( avatar_names[i], avatar_images[i]);
+        Character ch(avatar_names[i], avatar_images[i]);
         characterS.push_back(ch);
-
     }
 
-    std::vector<std::unique_ptr<sf::RectangleShape>> avas; 
+    std::vector<std::unique_ptr<sf::RectangleShape>> avas;
     // for(int i=0;i<characterS.size();i++)
     // {
 
     // }
-    avas.push_back(std::make_unique<sf::RectangleShape>(sf::Vector2f(ava_rect_size.first, ava_rect_size.second)) );
+    avas.push_back(std::make_unique<sf::RectangleShape>(sf::Vector2f(ava_rect_size.first, ava_rect_size.second)));
 
-    //sf::RectangleShape ava1(sf::Vector2f(ava_rect_size.first, ava_rect_size.second));
-    
-    std::pair<float, float> margin={20.0f,300.0f + 20.0f};
+    // sf::RectangleShape ava1(sf::Vector2f(ava_rect_size.first, ava_rect_size.second));
+
+    std::pair<float, float> margin = {20.0f, 300.0f + 20.0f};
     avas[0]->setPosition(sf::Vector2f(margin.first, margin.second));
-    
 
-    std::pair<float, float> margin_act=margin;
-    for(int i=1;i<characterS.size();i++)
+    std::pair<float, float> margin_act = margin;
+    for (int i = 1; i < characterS.size(); i++)
     {
-        
-        sf::Vector2f vec_pos = avas[i-1]->getPosition();
-        std::pair<float, float> pos={vec_pos.x, vec_pos.y};
-        
 
-        bool isFirstRow;
-        if(margin_act.first < 400-ava_rect_size.first)
-        {   
-            isFirstRow=true;
+        sf::Vector2f vec_pos = avas[i - 1]->getPosition();
+        std::pair<float, float> pos = {vec_pos.x, vec_pos.y};
+
+        bool isFirstRow = false;
+        if (margin_act.first < 400 - ava_rect_size.first)
+        {
+            isFirstRow = true;
             margin_act.first = pos.first + ava_rect_size.first + margin.first;
             margin_act.second = margin.second;
 
-            avas.push_back(std::make_unique<sf::RectangleShape>(sf::RectangleShape(sf::Vector2f(ava_rect_size.first, ava_rect_size.second) ) ));
+            avas.push_back(std::make_unique<sf::RectangleShape>(sf::RectangleShape(sf::Vector2f(ava_rect_size.first, ava_rect_size.second))));
             avas[i]->setPosition(sf::Vector2f(margin_act.first, margin_act.second));
-
-
         }
 
         else
         {
-            if(isFirstRow) 
+            if (isFirstRow)
             {
-                margin_act.first=margin.first;
-                margin_act.second+=ava_rect_size.second+margin.second;
+                margin_act.first = margin.first;
+                margin_act.second += ava_rect_size.second + margin.second;
 
                 avas.push_back(std::make_unique<sf::RectangleShape>(sf::RectangleShape(sf::Vector2f(ava_rect_size.first, ava_rect_size.second))));
                 avas[i]->setPosition(sf::Vector2f(margin_act.first, margin_act.second));
-                isFirstRow=false;
-                               
+                isFirstRow = false;
             }
 
-            else 
+            else
             {
                 margin_act.first = pos.first + ava_rect_size.first + margin.first;
                 margin_act.second = margin.second;
@@ -75,20 +68,11 @@ void init_char_select(MainMenuModule *main_menu_ptr)
                 avas.push_back(std::make_unique<sf::RectangleShape>(sf::RectangleShape(sf::Vector2f(ava_rect_size.first, ava_rect_size.second))));
                 avas[i]->setPosition(sf::Vector2f(margin_act.first, margin_act.second));
             }
-
-
-
         }
-
-        
-
     }
 
-
-    for(int i=0;i<avas.size();i++)
-        {
-            main_menu_ptr->_game.shapes.emplace_back(std::move(avas[i]));
-        }
-
-
+    for (int i = 0; i < avas.size(); i++)
+    {
+        main_menu_ptr->_game.shapes.emplace_back(std::move(avas[i]));
+    }
 }
